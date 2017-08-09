b95b617ca79c66e6e2402eb140099ca2f7c9dfc9
gustavosysm <gustavo.santos@systemsmobile.com.br>
2017-08-09 09:37:27 -0300

    Passing data between view controllers.

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Gustavo M Santos on 09/08/17.
+//  Copyright © 2017 Gustavo M Santos. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
